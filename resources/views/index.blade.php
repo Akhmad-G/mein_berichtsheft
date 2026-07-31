fb2ab1ea9c757628e10cc03085adffbfdd6e436f
akhmed.gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-07-31 12:58:47 +0200

    first steps

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Mein Berichtsheft')
+
+@section('content')
+  <nav>
+    <a href="/">Neu Tagesbericht</a>
+    <a href="/">Neu Wochenbericht</a>
+  </nav>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("berichtshefte.index");
 });
 
-Route::get('https://mein-berichtsheft.ddev.artif.dev/', function () {
-    return view('welcome');
-});
+Route::get('/berichtshefte/', function () {
+    return view('index');
+})->name("berichtshefte.index");
 
+Route::view()
